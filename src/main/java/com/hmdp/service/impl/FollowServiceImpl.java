a09352e5adfd090d7a21ae18012425616f1c930e
Garixx <18316304319@163.com>
2025-12-25 17:17:34 +0800

    好友关注-关注和取关

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,9 +1,12 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +20,44 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    /**
+     * 关注和取关
+     * @param followUserId
+     * @param isFollow
+     * @return
+     */
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        //1.获取当前登录用户
+        Long userId = UserHolder.getUser().getId();
+        //2.判断是要关注还是取消关 如果isFollow为true则关注，为false则取消关
+        if (isFollow){
+            //3.关注，将关注信息插入数据库
+            Follow follow = new Follow();
+            follow.setFollowUserId(followUserId);
+            follow.setUserId(userId);
+            save( follow);
+        }else {
+            //4.取关，将关注信息从数据库中删除 delect from follow where user_id = ? and follow_user_id = ?
+            remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId).eq("follow_user_id", followUserId));
+        }
+        return Result.ok();
+    }
+
+    /**
+     * 查询是否关注
+     * @param followUserId
+     * @return
+     */
+    @Override
+    public Result isFollow(Long followUserId) {
+        //判断登录用户是否关注该用户，其实就是判断数据库中是否有该记录 select * from follow where user_id = ? and follow_user_id = ?
+        //1.获取登录用户
+        Long userId = UserHolder.getUser().getId();
+        //2.查询是否关注
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+        //3.判断并返回
+        return Result.ok(count > 0);
+    }
 }
